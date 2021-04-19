7124a7d6dafa93131b1d134a5b67f9282b10f64d
Praveen <praveenkumarnampally@gmail.com>
2021-04-19 15:22:51 +0530

    Initioal Commit

---
+++ src/main/java/net/inetsolve/MocSbDatajpaApplication.java
@@ -0,0 +1,13 @@
+package net.inetsolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocSbDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocSbDatajpaApplication.class, args);
+	}
+
+}
